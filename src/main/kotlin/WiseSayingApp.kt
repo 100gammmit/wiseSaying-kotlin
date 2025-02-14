b7136de391d84947ffe17996e47fc3a232b44aec
100gammmit <100minha@naver.com>
2025-02-14 14:36:37 +0900

    에러 핸들러 적용

--- src/main/kotlin/WiseSayingApp.kt
+++ src/main/kotlin/WiseSayingApp.kt
@@ -1,10 +1,10 @@
 package org.example
 
+import org.example.global.ExceptionHandler
 import org.example.global.Parser
 import org.example.global.exception.CommandErrorCode
 import org.example.global.exception.CommandException
 import org.example.wiseSaying.controller.WiseSayingController
-import org.example.wiseSaying.exception.WiseSayingException
 
 /**
  * ${NAME}
@@ -14,29 +14,25 @@ import org.example.wiseSaying.exception.WiseSayingException
 fun main() {
 
     val wiseSayingController = WiseSayingController()
+    val exceptionHandler = ExceptionHandler()
     val parser = Parser()
+    var appRunning = true
 
-    while(true) {
+    while(appRunning) {
 
-        println("== 명언 앱 ==")
-        print("명령) ")
-        val command = readln()
+        exceptionHandler.handleGlobalException {
+            println("== 명언 앱 ==")
+            print("명령) ")
+            val command = readln()
 
-        try{
             when {
-                command == "종료" -> break;
+                command == "종료" -> appRunning = false
                 command == "등록" -> wiseSayingController.save()
                 command.startsWith("목록") -> wiseSayingController.getAll()
                 command.startsWith("삭제") -> wiseSayingController.delete(parser.parse(command))
                 command.startsWith("수정") -> wiseSayingController.modify(parser.parse(command))
                 else -> throw CommandException(CommandErrorCode.INVALID_COMMAND)
             }
-        } catch (e: CommandException) {
-            println(e.message)
-        } catch (e: WiseSayingException) {
-            println(e.message)
-        } catch (e: Exception) {
-            e.printStackTrace()
         }
 
     }
